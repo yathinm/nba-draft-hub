d34927762b333a3e30d687fcde401858e22da5b0
yathinm <yathinmrudul@gmail.com>
2025-05-18 23:55:05 -0700

    Fix: assert non-null root element for Vercel build

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { CssBaseline } from '@mui/material';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <CssBaseline />
     <App />
